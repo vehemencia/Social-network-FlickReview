e612e619da4b30d8db121d91deec489a29232eea
Fernanda Alvarez <mariafernandalvarezramirez@gmail.com>
2022-08-29 18:21:20 -0500

    Adding nav bar and SPA trial

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,3 +4,7 @@ export const myFunction = () => {
   // aqui tu codigo
   console.log('Hola mundo!');
 };
+
+export const exampleForHome = `<h1>Home page</h1>`
+
+export const exampleForProfile = `<h1>Profile page</h1>`
